c3c5c60399f4a149e2562c7e127d3baef0006998
AleixMJ <aleixtechIT@gmail.com>
2026-06-17 10:24:00 +0200

    added append function

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 class LinkedList {
     constructor() {
         this.head = null;
+        this.tail = null;
+    }
+
+    append(value) {
+
+        const newNode = new Node(value);
+        if(this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {            
+            this.tail.nextNode = newNode;
+            this.tail = newNode;
+        }
     }
 
 };
